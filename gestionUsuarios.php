d1ef58d8cfc645beb01c9355c3593547be395002
Mateo Biondi <mateobiondi05@gmail.com>
2024-09-23 20:30:58 -0300

    Agrego a este archivo una mejora en la autenticación

--- gestionUsuarios.php
+++ gestionUsuarios.php
@@ -1,2 +1,3 @@
 <?php
 echo "En este archivo voy a gestionar la autenticacion y los datos de los usuarios";
+echo "Ahora, para mejorar la seguridad y autenticación de los usuarios, van a tener que resolver un captcha";
